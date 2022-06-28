6bb77bb03a5c470941774ac1c6af1e4eb764e682
neige-i <ibrahim.naji.rachdi@gmail.com>
2022-06-28 14:39:57 +0200

    Import Hilt library

--- app/src/main/java/fr/neige_i/fdj_entretien/MainActivity.kt
+++ app/src/main/java/fr/neige_i/fdj_entretien/MainActivity.kt
@@ -2,8 +2,10 @@ package fr.neige_i.fdj_entretien
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
 import fr.neige_i.fdj_entretien.databinding.ActivityMainBinding
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
