28c769f96909c0cdca71f823c45cd55660905707
neige-i <ibrahim.naji.rachdi@gmail.com>
2022-06-30 16:26:36 +0200

    Add HTTP logging interceptor

--- app/src/main/java/fr/neige_i/fdj_entretien/di/DataProvidingModule.kt
+++ app/src/main/java/fr/neige_i/fdj_entretien/di/DataProvidingModule.kt
@@ -6,6 +6,8 @@ import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import fr.neige_i.fdj_entretien.BuildConfig
 import fr.neige_i.fdj_entretien.data.sport_api.SportDataSource
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -17,6 +19,15 @@ object DataProvidingModule {
     fun provideSportDatasource(): SportDataSource = Retrofit.Builder()
         .baseUrl("https://www.thesportsdb.com/api/v1/json/${BuildConfig.THE_SPORTS_DB_API_KEY}/")
         .addConverterFactory(GsonConverterFactory.create())
+        .client(
+            OkHttpClient.Builder()
+                .addInterceptor(
+                    HttpLoggingInterceptor().apply {
+                        setLevel(HttpLoggingInterceptor.Level.BASIC)
+                    }
+                )
+                .build()
+        )
         .build()
         .create(SportDataSource::class.java)
 }
