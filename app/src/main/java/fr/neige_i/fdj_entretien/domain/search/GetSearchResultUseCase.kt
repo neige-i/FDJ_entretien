c553b3cb7663a76793a52302b71dd7d8ff05644d
neige-i <ibrahim.naji.rachdi@gmail.com>
2022-07-15 01:50:49 +0200

    Introduce Turbine

--- app/src/main/java/fr/neige_i/fdj_entretien/domain/search/GetSearchResultUseCase.kt
+++ app/src/main/java/fr/neige_i/fdj_entretien/domain/search/GetSearchResultUseCase.kt
@@ -6,13 +6,17 @@ import fr.neige_i.fdj_entretien.data.sport_api.SportRepository
 import fr.neige_i.fdj_entretien.data.sport_api.model.NetworkResult
 import fr.neige_i.fdj_entretien.data.sport_api.model.TeamResponse
 import fr.neige_i.fdj_entretien.domain.DataResult
+import fr.neige_i.fdj_entretien.util.CoroutineDispatcherProvider
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOn
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetSearchResultUseCase @Inject constructor(
     private val sportRepository: SportRepository,
     private val searchRepository: SearchRepository,
+    private val dispatcherProvider: CoroutineDispatcherProvider,
 ) {
 
     operator fun invoke(): Flow<DataResult<List<TeamResponse>>> = searchRepository.getSearchedLeagueNameFlow()
@@ -20,8 +24,21 @@ class GetSearchResultUseCase @Inject constructor(
             // STEP 2: API call
             when (val teamsResult = sportRepository.getSoccerTeamsByLeague(searchedLeagueName)) {
                 is NetworkResult.Success -> DataResult.Content(data = teamsResult.content)
-                is NetworkResult.Failure.IoFailure -> DataResult.Error(errorMessage = R.string.load_team_list_error)
+                is NetworkResult.Failure.IoFailure -> {
+                    var failureCount = 0
+
+                    while (failureCount++ < 4) {
+                        delay(1_000L * failureCount)
+                        val newTeamsResult = sportRepository.getSoccerTeamsByLeague(searchedLeagueName)
+
+                        if (newTeamsResult is NetworkResult.Success) {
+                            return@map DataResult.Content(data = newTeamsResult.content)
+                        }
+                    }
+
+                    DataResult.Error(errorMessage = R.string.load_team_list_error)
+                }
                 is NetworkResult.Failure.ApiFailure -> DataResult.Error(errorMessage = R.string.team_list_api_error)
             }
-        }
+        }.flowOn(dispatcherProvider.io)
 }
--- app/src/test/java/fr/neige_i/fdj_entretien/domain/search/GetSearchResultUseCaseTest.kt
+++ app/src/test/java/fr/neige_i/fdj_entretien/domain/search/GetSearchResultUseCaseTest.kt
@@ -1,5 +1,6 @@
 package fr.neige_i.fdj_entretien.domain.search
 
+import app.cash.turbine.test
 import fr.neige_i.fdj_entretien.R
 import fr.neige_i.fdj_entretien.TestCoroutineRule
 import fr.neige_i.fdj_entretien.data.search.SearchRepository
@@ -10,6 +11,7 @@ import fr.neige_i.fdj_entretien.domain.DataResult
 import io.mockk.*
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.currentTime
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Before
@@ -23,20 +25,24 @@ class GetSearchResultUseCaseTest {
 
     private val sportRepository: SportRepository = mockk()
     private val searchRepository: SearchRepository = mockk()
+    private val coroutineDispatcherProvider = testCoroutineRule.getCoroutineDispatcherProvider()
 
-    private val subject = GetSearchResultUseCase(sportRepository, searchRepository)
+    private val subject = GetSearchResultUseCase(sportRepository, searchRepository, coroutineDispatcherProvider)
 
     @Before
     fun setUp() {
-        every { searchRepository.getSearchedLeagueNameFlow() } returns flowOf("leagueName")
-        coEvery { sportRepository.getSoccerTeamsByLeague("leagueName") } returns NetworkResult.Success(content = defaultList)
+        every { searchRepository.getSearchedLeagueNameFlow() } returns flowOf(DEFAULT_LEAGUE_NAME)
+        coEvery { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) } returns NetworkResult.Success(content = DEFAULT_LIST)
     }
 
     @After
     fun tearDown() {
-        verify(exactly = 1) { searchRepository.getSearchedLeagueNameFlow() }
-        coVerify { sportRepository.getSoccerTeamsByLeague("leagueName") }
-        confirmVerified(sportRepository, searchRepository)
+        verify(exactly = 1) {
+            searchRepository.getSearchedLeagueNameFlow()
+            coroutineDispatcherProvider.io
+        }
+        coVerify { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) }
+        confirmVerified(sportRepository, searchRepository, coroutineDispatcherProvider)
     }
 
     @Test
@@ -45,13 +51,13 @@ class GetSearchResultUseCaseTest {
         val result = subject.invoke().first()
 
         // THEN
-        assertEquals(DataResult.Content(data = defaultList), result)
+        assertEquals(DataResult.Content(data = DEFAULT_LIST), result)
     }
 
     @Test
     fun `edge case - return error when API call fails with IO exception`() = testCoroutineRule.runTest {
         // GIVEN
-        coEvery { sportRepository.getSoccerTeamsByLeague("leagueName") } returns NetworkResult.Failure.IoFailure
+        coEvery { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) } returns NetworkResult.Failure.IoFailure
 
         // WHEN
         val result = subject.invoke().first()
@@ -63,7 +69,7 @@ class GetSearchResultUseCaseTest {
     @Test
     fun `edge case - return error when API call fails with API exception`() = testCoroutineRule.runTest {
         // GIVEN
-        coEvery { sportRepository.getSoccerTeamsByLeague("leagueName") } returns NetworkResult.Failure.ApiFailure
+        coEvery { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) } returns NetworkResult.Failure.ApiFailure
 
         // WHEN
         val result = subject.invoke().first()
@@ -72,7 +78,56 @@ class GetSearchResultUseCaseTest {
         assertEquals(DataResult.Error(errorMessage = R.string.team_list_api_error), result)
     }
 
-    private fun getDefaultTeamsResponse(): List<TeamResponse> = List(5) { mockk() } // Arbitrary content
+    @Test
+    fun `edge case - connectivity is bad, success after 2 tries`() = testCoroutineRule.runTest {
+        // GIVEN
+        coEvery { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) } returnsMany listOf(
+            NetworkResult.Failure.IoFailure,
+            NetworkResult.Failure.IoFailure,
+            NetworkResult.Success(content = DEFAULT_LIST)
+        )
 
-    private val defaultList = getDefaultTeamsResponse()
+        // WHEN
+        subject.invoke().test {
+            // THEN
+            assertEquals(DataResult.Content(data = DEFAULT_LIST), awaitItem())
+            assertEquals(3_000, currentTime)
+            awaitComplete()
+
+            verify(exactly = 1) {
+                searchRepository.getSearchedLeagueNameFlow()
+                coroutineDispatcherProvider.io
+            }
+            coVerify(exactly = 3) { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) }
+            confirmVerified(searchRepository, sportRepository, coroutineDispatcherProvider)
+        }
+    }
+
+    @Test
+    fun `error case - connectivity is atrocious, failure after 5 tries`() = testCoroutineRule.runTest {
+        // GIVEN
+        coEvery { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) } returns NetworkResult.Failure.IoFailure
+
+        // WHEN
+        subject.invoke().test {
+            // THEN
+            assertEquals(DataResult.Error(errorMessage = R.string.load_team_list_error), awaitItem())
+            assertEquals(10_000, currentTime)
+            awaitComplete()
+
+            verify(exactly = 1) {
+                searchRepository.getSearchedLeagueNameFlow()
+                coroutineDispatcherProvider.io
+            }
+            coVerify(exactly = 5) { sportRepository.getSoccerTeamsByLeague(DEFAULT_LEAGUE_NAME) }
+            confirmVerified(searchRepository, sportRepository, coroutineDispatcherProvider)
+        }
+    }
+
+    companion object {
+        private const val DEFAULT_LEAGUE_NAME = "leagueName"
+        private val DEFAULT_LIST = getDefaultTeamsResponse()
+
+        private fun getDefaultTeamsResponse(): List<TeamResponse> = List(5) { mockk() } // Arbitrary content
+    }
 }
